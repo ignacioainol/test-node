7a4d76d8bf2cd87f5c81f47f6b330c234efea8d6
Ignacio <shoik.xtazy@gmail.com>
2019-07-13 14:45:42 -0400

    fix put cuz was missing this char '?' in form

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -27,7 +27,19 @@ router.delete('/notes/delete/:id', async (req,res) => {
 
 router.get('/notes/edit/:id', async (req,res) => {
     const note = await Note.findById(req.params.id);
-    res.render('notes/edit-note');
+    res.render('notes/edit-note', { note });
+});
+
+// router.put('/notes/edit-note/:id', async (req,res ) => {
+//     const { title, description} = req.body;
+//     await Note.findByIdAndUpdate(req.params.id, {title, description});
+//     res.redirect('/notes');
+// });
+
+router.put('/notes/edit-note/:id', async (req,res) => {
+	const { title, description} = req.body;
+	await Note.findByIdAndUpdate(req.params.id, {title,description});
+	res.redirect('/notes');
 });
 
 module.exports = router;
